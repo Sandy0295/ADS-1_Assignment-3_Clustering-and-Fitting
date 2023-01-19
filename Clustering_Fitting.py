2314078bda2ce0b23d729123f202f7d4dd5f5347
Sandy0295 <116498686+Sandy0295@users.noreply.github.com>
2023-01-19 13:01:15 +0000

    Reading the Dataset and performing preprocessing steps

--- Clustering_Fitting.py
+++ Clustering_Fitting.py
@@ -59,3 +59,15 @@ def exponen_graph(t, n0, g):
     t = t - 1960.0
     f = n0 * np.exp(g*t)
     return f
+
+#calling the function that produces two dataframes: one with countries as columns and another with years as columns
+df_countries, df_years= read_data_file('C:/Users/sande/MS-DS/API_19_DS2_en_csv_v2_4700503-Copy.csv',['Afghanistan','Albania','Argentina','Austria','Belgium','Bangladesh','Brazil','Canada','Switzerland','Chile','China','Colombia','Denmark','Dominican Republic','Algeria','Spain','Finland','Fiji','France','United Kingdom','Greece','Greenland','Hungary','Indonesia','India','Ireland','Iraq','Iceland','Israel','Italy','Jamaica','Japan','Lebanon','Luxembourg','Morocco','Mexico','Myanmar','Netherlands','New Zealand','Pakistan','Peru','Poland','Romania','Russian Federation','Sweden','Thailand','Tunisia','Turkiye','Uruguay','United States','Vietnam','South Africa','Zimbabwe'])
+
+#selecting only few idnicators for clustering and fitting purpose
+selected_indicators = ['Urban population growth (annual %)','CO2 emissions from liquid fuel consumption (% of total)','CO2 emissions from solid fuel consumption (% of total)']
+
+#filtering the original dataset with the selected indicators
+selected_df = df_countries.loc[df_countries["Indicator Name"].isin(selected_indicators)]
+
+#dropped two columns for clustering purpose
+selected_df = selected_df.drop(columns=['Country Name','Indicator Name',],axis=1)
