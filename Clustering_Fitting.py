474ecea5a0c8970f6b71963da34adba136cba88d
Sandy0295 <116498686+Sandy0295@users.noreply.github.com>
2023-01-19 12:53:41 +0000

    Importing the required modules

---
+++ Clustering_Fitting.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 18 18:14:30 2023
+
+@author: sande
+"""
+
+#importing pandas module to read the file(data set) and to calculate the statistical property(Describe)
+import pandas as pd
+
+#importing numpy module to calculate the statistical properties (mean and standard deviation)
+import numpy as np
+
+#importing pyplot from matplotlib module to plot the visualization graphs
+import matplotlib.pyplot as plt
+
+#importing KMeans from sklearn object to identify the clusters
+from sklearn.cluster import KMeans
+
+#importing LabelEncoder to encode the categories of the data
+from sklearn.preprocessing import LabelEncoder
+
+#importing the custom error package 
+import err_ranges as error
+
+#importing curve_fit from scipy module to predict the population groth of Australia
+from scipy.optimize import curve_fit
